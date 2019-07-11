b642301363d1b9848dd607b4fa6ef6c7b78abace
michalsurowiec <michalsurowiec94@o2.pl>
2019-07-11 12:56:51 +0200

    Configuration of Spring Data

--- src/main/java/michalsurowiec/AppConfig.java
+++ src/main/java/michalsurowiec/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
@@ -21,6 +22,7 @@ import javax.persistence.EntityManagerFactory;
 @EnableWebMvc
 @ComponentScan
 @EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "michalsurowiec")
 public class AppConfig extends WebMvcConfigurerAdapter {
 
     @Bean
